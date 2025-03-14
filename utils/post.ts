e9afb4af1ef053549d91193861a1cc423e531186
bloodnighttw <bbeenn1227@gmail.com>
2025-03-14 09:22:29 +0800

    Update package.json

--- utils/post.ts
+++ utils/post.ts
@@ -1,14 +1,15 @@
-import { defineContent, zod as z} from "@mkblog/core";
+import { defineContent, zod as z } from "@mkblog/core";
 import tocPlugin from "@mkblog/toc";
 import remarkGfm from "remark-gfm";
 import previewImage from "@/utils/previewImage";
+import { console } from "inspector";
 
 const schema = {
 	title: z.string(),
 	categories: z.array(z.string()).default([]),
 	date: z.string().pipe(z.coerce.date()),
-	draft: z.boolean().optional()
-}
+	draft: z.boolean().optional(),
+};
 
 const _zobject = z.object(schema);
 
@@ -17,13 +18,8 @@ export type PostMetadata = z.infer<typeof _zobject>;
 const contents = await defineContent({
 	include: "./posts/**/*.md", // glob pattern
 	schema,
-	mkBlogPlugins: [
-		tocPlugin,
-		previewImage
-	],
-	remarkPlugins: [
-		remarkGfm
-	]
-})
+	mkBlogPlugins: [tocPlugin, previewImage],
+	remarkPlugins: [remarkGfm],
+});
 
 export default contents;
