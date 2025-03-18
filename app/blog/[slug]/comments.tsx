7dbc65d1794636f8009c7d7d957a205fcc917123
bloodnighttw <bbeenn1227@gmail.com>
2025-03-19 05:22:15 +0800

    feat: update url.

--- app/blog/[slug]/comments.tsx
+++ app/blog/[slug]/comments.tsx
@@ -5,6 +5,6 @@ import Giscus from "@giscus/react";
 export default function Comments(){
 
 	return <div>
-		<Giscus repo="bloodnighttw/Giscus" repoId="R_kgDOOK1SwQ" mapping="pathname" categoryId="DIC_kwDOOK1Swc4CoMm7" theme="http://localhost:3000/giscus.css"/>
+		<Giscus repo="bloodnighttw/Giscus" repoId="R_kgDOOK1SwQ" mapping="pathname" categoryId="DIC_kwDOOK1Swc4CoMm7" theme="https://bntw.dev/giscus.css"/>
 	</div>
 }
