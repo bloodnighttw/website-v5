4dd21be8dd8a7238bdfe1160d20ae5d5757067ef
bloodnighttw <bbeenn1227@gmail.com>
2025-03-21 00:32:25 +0800

    feat: giscus theme.

--- app/blog/[slug]/comments.tsx
+++ app/blog/[slug]/comments.tsx
@@ -5,6 +5,13 @@ import Giscus from "@giscus/react";
 export default function Comments(){
 
 	return <div>
-		<Giscus repo="bloodnighttw/Giscus" repoId="R_kgDOOK1SwQ" mapping="pathname" categoryId="DIC_kwDOOK1Swc4CoMm7" theme="https://bntw.dev/giscus.css"/>
+		<Giscus
+			repo="bloodnighttw/Giscus"
+			repoId="R_kgDOOK1SwQ"
+			mapping="pathname"
+			categoryId="DIC_kwDOOK1Swc4CoMm7"
+			theme="http://localhost:3000/giscus.css"
+			reactionsEnabled="0"
+			inputPosition="top"/>
 	</div>
 }
