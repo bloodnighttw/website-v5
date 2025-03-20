54e6ddb80d3fee056b09a8e60347d14e321db543
bloodnighttw <bbeenn1227@gmail.com>
2025-03-21 03:30:10 +0800

    feat: using link instead in card title component.

--- compoments/Card/CardTitle.tsx
+++ compoments/Card/CardTitle.tsx
@@ -4,7 +4,7 @@ export function CardTitle({ title, url }: { title: string, url?: string }) {
 	return (
 		<div className="flex items-end">
 			<p className="text-secondary mr-auto text-2xl">{title}</p>
-			<a className="text-secondary hover:underline" href={url ?? "/"}>See More</a>
+			<Link className="text-secondary hover:underline" href={url ?? "/"}>See More</Link>
 		</div>
 	)
 }
