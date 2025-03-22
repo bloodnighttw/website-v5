ef4d8c1b39de3daaeb14315d4c156a064a09f926
bloodnighttw <bbeenn1227@gmail.com>
2025-03-22 16:38:27 +0800

    fix: fix linter is not config correctly.

--- compoments/Card/Card.tsx
+++ compoments/Card/Card.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Card({href, preview, title} : {href: URL, preview: string, title: string}) {
+export default function Card({href, preview, title} : {href: URL | string, preview: string, title: string}) {
 	return (
 		<Link href={href}
 			className="rounded basis-auto overflow-hidden border-[0.1px] border-bsecondary w-full *:not-first:mt-4 *:not-first:p-4 hover:scale-110 duration-200 bg-bprimary hover:z-[100]">
