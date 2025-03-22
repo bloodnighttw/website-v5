8dd88ced5082a83ceb881bc16cf36fb6a995b0d7
bloodnighttw <bbeenn1227@gmail.com>
2025-03-22 19:38:56 +0800

    chore: used fill instead in card.

--- compoments/Card/Card.tsx
+++ compoments/Card/Card.tsx
@@ -5,7 +5,10 @@ export default function Card({href, preview, title} : {href: URL | string, previ
 	return (
 		<Link href={href}
 			className="rounded basis-auto overflow-hidden border-[0.1px] border-bsecondary w-full *:not-first:mt-4 *:not-first:p-4 hover:scale-110 duration-200 bg-bprimary hover:z-[100]">
-			<Image src={preview} alt="preview" className="w-full h-36 object-cover" height={144} loading="eager"/>
+			<div className="relative h-36">
+				{/* fill={true} will make component into absolute */}
+				<Image src={preview} alt="preview" className="object-cover" fill={true} loading="eager"/>
+			</div>
 			<p className="text-2xl font-bold">{title}</p>
 		</Link>
 	)
