ad676d89f68a9657f81315159b511251997027d2
bloodnighttw <bbeenn1227@gmail.com>
2025-03-27 09:59:49 +0800

    chore: add tailwind merage.

---
+++ utils/cn.tsx
@@ -0,0 +1,8 @@
+import clsx, { ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...classes: ClassValue[]) {
+	return twMerge(clsx(...classes))
+}
+
+export default cn;
