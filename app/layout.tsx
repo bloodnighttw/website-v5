e7aad819041cccc3515b1fa1667b4b333e080926
bloodnighttw <bbeenn1227@gmail.com>
2025-03-28 09:19:32 +0800

    fix: nextjs error.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<meta http-equiv="Delegate-CH" content="sec-ch-dpr https://bntw.dev; sec-ch-viewport-width https://bntw.dev"/>
+				<meta httpEquiv="Delegate-CH" content="sec-ch-dpr https://bntw.dev; sec-ch-viewport-width https://bntw.dev"/>
 			</head>
 			<body className={`$antialiased`}>
 				<NavPanel>
