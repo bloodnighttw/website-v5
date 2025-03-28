a04afd57d46de2c2eaf828406377f3aa0a26d548
bloodnighttw <bbeenn1227@gmail.com>
2025-03-28 09:33:17 +0800

    revert: improve csp.

--- compoments/Blog/ArticleCard.tsx
+++ compoments/Blog/ArticleCard.tsx
@@ -14,7 +14,7 @@ export default function ArticleCard(props:Props) {
 			className="rounded basis-auto overflow-hidden border-[0.1px] border-bsecondary w-full *:not-first:mt-4 *:not-first:p-4 hover:scale-110 duration-200 bg-bprimary hover:z-[100]">
 			<div className="relative h-36">
 				{/* fill={true} will make component into absolute */}
-				<Image src={props.info.preview} alt="preview" className="object-cover" fill={true} loading="eager" priority={true} />
+				<Image src={props.info.preview} alt="preview" className="object-cover" fill={true} loading="eager"/>
 			</div>
 			<p className="text-2xl font-bold">{props.info.title}</p>
 		</Link>
