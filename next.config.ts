2c870d0f149ec82ad743ccdbe96505a34277e3fe
bloodnighttw <bbeenn1227@gmail.com>
2025-04-02 02:22:19 +0800

    refactor: enhance MDX configuration with additional remark and rehype plugins

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,14 @@ import { withContentCollections } from "@content-collections/next";
 import createMDX from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+import { remarkNextImage } from "@/utils/remark-plugin/next-tImage";
+import rehypeStarryNight from "rehype-starry-night";
+import rehypeLineNumbers from "@/utils/rehype-plugin/line-numbers";
 
 const withMDX = createMDX({
 	options: {
-		remarkPlugins: [remarkFrontmatter,remarkMdxFrontmatter],
-		rehypePlugins: [],
+		remarkPlugins: [remarkFrontmatter,remarkMdxFrontmatter,remarkNextImage],
+		rehypePlugins: [rehypeStarryNight,rehypeLineNumbers],
 	},
 });
 
