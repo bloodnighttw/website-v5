6426882233005f9aadea264fcea04bbf704f3fee
bloodnighttw <bbeenn1227@gmail.com>
2025-04-14 22:40:32 +0800

    fix: improve performance and fix panel will close when click.

--- compoments/Blog/TocLayout.tsx
+++ compoments/Blog/TocLayout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect} from "react";
+import React, { useCallback, useEffect } from "react";
 import cn from "@/utils/cn";
 import { ListSvg, ToTop } from "@/app/assets/svg";
 import Part from "@/compoments/Part";
@@ -26,7 +26,7 @@ function Toc(prop: TocProp) {
 
 		const progressValue = prop.progressRef.current;
 
-		setProgress(progressValue);
+		setProgress(progressValue > 100 ? 100 : progressValue);
 
 	},[prop.progressRef]);
 
@@ -38,29 +38,31 @@ function Toc(prop: TocProp) {
 		};
 	}, [handleScroll]);
 
-
-
 	useEffect(() => {
-		if (progress === 100) {
+		if(progress === 100){
 			setOpen(false);
 		}
 	}, [progress]);
 
-	const handleClick = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
-		if(!open && progress === 100){
-			// scroll this div to the bottom
-			e.currentTarget.scrollIntoView({
-				behavior: "smooth",
-				block: "end",
-			})
-		}
+	const handleClick = useCallback(() => {
 		setOpen(!open);
-	},[open, progress])
+		if(!open){
+			divRef.current?.scrollIntoView({
+				behavior: "smooth",
+				block: "end"
+			});
+			return;
+		}
+
+	},[open])
 
 	const clickOutside = useCallback((e: MouseEvent) => {
+
 		if (!divRef.current) return;
+
 		if (!divRef.current.contains(e.target as Node)) {
 			setOpen(false);
+			console.log("click outside");
 		}
 	},[]);
 
@@ -73,10 +75,10 @@ function Toc(prop: TocProp) {
 
 
 	return (
-		<div ref={divRef}>
+		<div className="scroll-mb-2" ref={divRef}>
 			<div
 				className={cn(
-					"bg-bprimary h-8 w-8 rounded-full *:m-auto flex items-center justify-center scroll-mb-2",
+					"bg-bprimary h-8 w-8 rounded-full *:m-auto flex items-center justify-center",
 					"border border-dot cursor-pointer active:scale-90 duration-200 relative",
 				)}
 				onClick={handleClick}
@@ -94,12 +96,67 @@ function Toc(prop: TocProp) {
 	)
 }
 
-type styleState = -1 | 0 | 1 | 2 ;
+
+interface GoToTopProps {
+	progressRef: React.RefObject<number>;
+}
+
+type StyleState = -1 | 0 | 1;
+
+function GoToTop({progressRef}: GoToTopProps) {
+
+	const [styleState, setStyleState] = React.useState<StyleState>(-1);
+
+	const handleScroll = useCallback(() => {
+
+		if(progressRef.current === -1) return;
+
+		const progressValue = progressRef.current;
+
+		if( progressValue <= 0){
+			if( styleState === -1)
+				return;
+			setStyleState(0)
+		} else if (progressValue > 0) {
+			setStyleState(1);
+		}
+
+	},[styleState, progressRef])
+
+	useEffect(() => {
+		// don't touch it, it's a hack to make the animation work
+		const task = setTimeout(()=>{
+			handleScroll();
+		}, 0);
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+			clearTimeout(task);
+		};
+	}, [handleScroll]);
+
+	return <div
+		className={cn(
+			"cursor-pointer active:scale-90 duration-200",
+			// to prevent the animation is triggered when first load
+			styleState === -1 && "hidden",
+			styleState === 0 && "fade-out",
+			styleState === 1 && "fade-in",
+		)}
+		// bring the element to the top
+		onClick={(e)=> {
+			e.preventDefault();
+			document.body.scrollIntoView({ behavior: "smooth" });
+		}}
+	>
+		{ToTop}
+	</div>
+}
 
 export default function Page(prop:LayoutProps) {
 
 	const ref = React.useRef<HTMLDivElement>(null);
-	const [styleStatus, setStyleStatus] = React.useState<styleState>(-1);
+	const [absolute, setAbsolute] = React.useState(true);
 	const progressRef = React.useRef(-1);
 
 	const handleProgress = useCallback(() => {
@@ -137,17 +194,12 @@ export default function Page(prop:LayoutProps) {
 
 		const progressValue = progressRef.current;
 
-		if( progressValue === 0){
-			if( styleStatus === -1)
-				return;
-			setStyleStatus(0)
-		} else if (progressValue > 0 && progressValue < 100) {
-			setStyleStatus(1);
-		} else if (progressValue >= 100) {
-			setStyleStatus(2);
+		if( progressValue < 100) {
+			setAbsolute(false);
+		} else {
+			setAbsolute(true);
 		}
-
-	},[styleStatus])
+	},[])
 
 	useEffect(() => {
 
@@ -171,34 +223,20 @@ export default function Page(prop:LayoutProps) {
 		<Toc progressRef={progressRef}/>
 	),[]);
 
+	const goToTopMemo = React.useMemo(() => (
+		<GoToTop progressRef={progressRef}/>
+	),[]);
+
 	const bottomPanelMemo = React.useMemo(() => {
 		return <div className={cn(
-			styleStatus === 2 && "absolute",
-			styleStatus <= 1 && "fixed z-100",
+			absolute ? "absolute" : "fixed z-100",
 			"duration-400 bottom-4 right-0 2xl:mr-[calc((-0.75rem+100svw-var(--size-width-max))/2)] mr-4 flex h-6 items-center gap-2"
 		)}
 		>
-			<div
-				className={cn(
-					"cursor-pointer active:scale-90 duration-200",
-					// to prevent the animation is triggered when first load
-					styleStatus === -1 && "hidden",
-					styleStatus === 0 && "fade-out",
-					styleStatus >= 1 && "fade-in",
-				)}
-				// bring the element to the top
-				onClick={(e)=> {
-					e.preventDefault();
-					if (!ref.current) return;
-					document.body.scrollIntoView({ behavior: "smooth" });
-				}}
-			>
-				{ToTop}
-			</div>
+			{goToTopMemo}
 			{TocMemo}
 		</div>
-	},[TocMemo, styleStatus]);
-
+	},[TocMemo, absolute, goToTopMemo]);
 
 	return <div ref={ref}>
 		{childrenMemo}
