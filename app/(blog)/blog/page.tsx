588142ba2bc911d1dcc836445abb9e5f67e176ac
bloodnighttw <bbeenn1227@gmail.com>
2025-04-17 05:42:45 +0800

    feat: add metadata in all blog post page.

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -3,6 +3,22 @@ import { ArticleCards } from "@/compoments/Blog/ArticleCard";
 import SecondaryPanel from "@/compoments/panel/SecondaryPanel";
 import Part from "@/compoments/Part";
 import { allPosts } from "content-collections";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "All Blog Post | Bloodnighttw",
+	openGraph: {
+		title: "Blog | Bloodnighttw",
+		type: "website",
+		images: [],
+	},
+	twitter: {
+		title: "Blog | Bloodnighttw",
+		site: "@bloodnighttw",
+		card: "summary_large_image",
+		images: [],
+	},
+}
 
 export default async function BlogPosts() {
 	const metadataWithPreview = allPosts.sort(
