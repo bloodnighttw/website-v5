335a7196a9cda880577f6f29a8b8cf646feeb961
bloodnighttw <bbeenn1227@gmail.com>
2025-04-30 19:33:37 +0800

    fix: correct JSX attribute casing for compatibility
    Updated `fill-rule` to `fillRule` to adhere to JSX attribute naming conventions. This ensures proper rendering and avoids potential React warnings or errors.

--- compoments/Blog/ArticleTitle.tsx
+++ compoments/Blog/ArticleTitle.tsx
@@ -20,7 +20,7 @@ export default function ArticleTitle({
 					href={url ?? "/"}
 				>
 					<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="fill-primary/60 hover:fill-primary h-6 w-6 duration-200" viewBox="0 0 16 16">
-						<path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+						<path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
 					</svg>
 				</Link>
 			)}
