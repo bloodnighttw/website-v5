97e5daa27e71fba994a7be6c8e422ed700f6b19e
bloodnighttw <bbeenn1227@gmail.com>
2025-04-30 16:19:38 +0800

    refactor: streamline ArticleCard styling and image handling
    Simplified class structures and enhanced hover and animation effects. Updated image properties for better build differentiation (debug vs. production). Improved overall code clarity and maintainability.

--- compoments/Blog/ArticleCard.tsx
+++ compoments/Blog/ArticleCard.tsx
@@ -10,19 +10,18 @@ export default function ArticleCard(props: Props) {
 	return (
 		<Link
 			href={"/blog/" + props.info.slug}
-			className="rounded basis-auto overflow-hidden border-[0.1px] border-bsecondary w-full *:not-first:mt-4 *:not-first:p-4 hover:scale-110 duration-200 bg-bprimary hover:z-[100]"
+			className="rounded group border border-bsecondary w-full duration-200 bg-bprimary hover:bg-bsecondary/20 hover:z-1 hover:-translate-y-1"
 		>
-			<div className="relative h-36">
-				{/* fill={true} will make component into absolute */}
-				<Image
-					src={props.info.preview ?? ""}
-					alt="preview"
-					className="object-cover"
-					fill={true}
-					loading="eager"
-				/>
-			</div>
-			<p className="text-2xl font-bold">{props.info.title}</p>
+			<Image
+				src={props.info.preview ?? ""}
+				alt="preview"
+				width={2000} // for debug build use
+				height={1600} // for debug build use
+				quality={0} // for production use
+				className="object-cover w-full h-40 border-b border-dot" //
+				loading="eager"
+			/>
+			<p className="text-2xl font-bold py-4 px-2 group-hover:pl-3 duration-200">{props.info.title}</p>
 		</Link>
 	);
 }
