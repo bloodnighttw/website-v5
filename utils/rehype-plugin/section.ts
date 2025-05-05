9de43f67eb9e3c25acb27d17feb990268030caf0
bloodnighttw <bbeenn1227@gmail.com>
2025-05-05 16:07:28 +0800

    fix: fix some page won't load since there has error when query id.

--- utils/rehype-plugin/section.ts
+++ utils/rehype-plugin/section.ts
@@ -3,14 +3,43 @@ import { Text as MText } from "mdast";
 import { selectAll } from "unist-util-select";
 
 type Text = HText | MText;
+function sanitizeHtmlId(input?: string) {
+	if (!input) return '';
+
+	// First character must be a letter (including Chinese characters)
+	let result = '';
+	const firstChar = input.charAt(0);
+
+	// Check if first character is a letter (including unicode letters like Chinese)
+	if (/^[a-zA-Z\u4e00-\u9fa5]$/.test(firstChar)) {
+		result = firstChar;
+	} else {
+		// If first character is invalid, start with "id-"
+		result = "id-";
+	}
+
+	// Process the rest of the string
+	for (let i = result === "id-" ? 0 : 1; i < input.length; i++) {
+		const char = input.charAt(i);
+		// Valid characters: letters (including Chinese), digits, hyphens, underscores, periods
+		if (/^[a-zA-Z0-9\u4e00-\u9fa5\-_.]$/.test(char)) {
+			result += char;
+		} else {
+			// Replace invalid chars with hyphen
+			result += "-";
+		}
+	}
+
+	return result;
+}
 
 export function generateId(map: Map<string, number>, text: Text[]): string {
 
-	const id = text
+	const combinedText = text
 		.map((it) => it.value)
 		.join()
-		// replace all spaces with -
-		.replace(/ /g, "-");
+
+	const id = sanitizeHtmlId(combinedText);
 
 	if (map.has(id)) {
 		map.set(id, map.get(id)! + 1);
