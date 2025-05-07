68a6b0558e69cf985d70477669cbef5f98ad80dc
bloodnighttw <bbeenn1227@gmail.com>
2025-05-07 20:17:43 +0800

    chore: add translate to self-introducing.

--- compoments/Home/Introducing.tsx
+++ compoments/Home/Introducing.tsx
@@ -10,56 +10,51 @@ import {
 } from "@/app/assets/svg";
 import Avatar from "@/compoments/avatar";
 import { svgUrl } from "@/utils/constant";
+import { getTranslations } from "next-intl/server";
+import TranslationWithIcon from "@/compoments/TranslationWithIcon";
 
-export default function Introducing() {
+export default async function Introducing() {
+
+	const t = await getTranslations("Introducing");
+
 	return (
 		<div className="flex flex-col-reverse lg:flex-row lg:flex-none gap-4 w-full my-auto lg:text-start text-center items-center lg:items-stretch">
 			<div className="lg:ml-0 lg:mr-auto my-auto lg:w-[calc(100%-240px)] max-w-[48rem] mx-auto">
 				<div className="text-center text-xl lg:text-start">
-					{"👋 hi!"}
+					{t("hi")}
 				</div>
-				<p className="text-4xl lg:text-6xl font-bold">
-					{"I'm bloodnighttw."}
+				<p className="text-4xl lg:text-6xl font-bold my-1.5">
+					{t("I'm")}	{t("name")}
 				</p>
 				<span className="font-mono typewriter">
-					21 y/o • Developer • Gamer{" "}
+					21 {t("age")} • Developer • Gamer{" "}
 				</span>
-				<p className="text-xl *:inline mt-2 *:mx-1">
-					{"I'm a developer who loves to create things."}
-					Familiar with
-					<Image
-						src={svgUrl["typescript"]}
-						alt="typescript"
-						width={24}
-						height={24}
-						loading="lazy"
-					/>
-					TypeScript,
-					<Image
-						src={svgUrl["react"]}
-						alt="react.js"
-						width={24}
-						height={24}
-						loading={"lazy"}
-					/>
-					React, and
-					<Image
-						src={svgUrl["java"]}
-						alt="java"
-						width={24}
-						height={24}
-						loading="lazy"
-					/>
-					Java, and also a
-					<Image
-						src={svgUrl["linux"]}
-						alt="linux"
-						width={24}
-						height={24}
-						loading="lazy"
-					/>
-					linux lover.
-				</p>
+				<TranslationWithIcon text={t("introducing")}
+					icon={{
+						"java" : <Image
+							src={svgUrl.java}
+							alt="Java"
+							width={24}
+							height={24}
+						/>,
+						"linux" : <Image
+							src={svgUrl.linux}
+							alt="Linux"
+							width={24}
+							height={24}/>,
+						"react" : <Image
+							src={svgUrl["react"]}
+							alt="React"
+							width={24}
+							height={24}/>,
+						"ts": <Image
+							src={svgUrl.typescript}
+							alt="TS"
+							width={24}
+							height={24}/>,
+					}}
+				/>
+
 				<div className="flex gap-6 lg:gap-8 mt-6 justify-center lg:justify-start">
 					<Link
 						href="https://github.com/bloodnighttw"
---
+++ compoments/TranslationWithIcon.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface Props {
+	text: string;
+	icon: {
+		[key: string]: React.ReactElement;
+	},
+	args?: Record<string, string | number | Date>;
+}
+
+// replace #key with the value(component) from text
+export default async function TranslationWithIcon(props: Props) {
+
+	const { text, icon } = props;
+	const result = text.split(/#(\w+)#/g).map((part, index) => {
+		const key = part;
+		const value = icon[key];
+		if (value) {
+			return <span key={index} className="*:inline">{value}</span>;
+		} else {
+			return <span key={index}>{key}</span>;
+		}
+	});
+
+
+
+	return (
+		<p className="text-xl mt-2">
+			{result}
+		</p>
+	);
+
+}
