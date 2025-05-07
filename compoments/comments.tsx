a4b2757b87a6039a42d59d539cff377e1b3a08d2
bloodnighttw <bbeenn1227@gmail.com>
2025-05-07 23:25:04 +0800

    feat: the comment will sync despite the different of language.

--- compoments/comments.tsx
+++ compoments/comments.tsx
@@ -2,14 +2,22 @@
 
 import Giscus from "@giscus/react";
 import { BASE_URL } from "@/utils/constant";
+import { usePathname } from "next/navigation";
 
 export default function Comments() {
+
+	const pathname = usePathname();
+
+	const lastSlashIndex = pathname.lastIndexOf("/");
+	const term = pathname.substring(lastSlashIndex + 1);
+
 	return (
 		<div>
 			<Giscus
 				repo="bloodnighttw/Giscus"
 				repoId="R_kgDOOK1SwQ"
-				mapping="pathname"
+				mapping="specific"
+				term={term}
 				categoryId="DIC_kwDOOK1Swc4CoMm7"
 				theme={`${BASE_URL}/giscus.css`}
 				reactionsEnabled="0"
