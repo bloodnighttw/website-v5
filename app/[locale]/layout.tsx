af251233619fa7cb66a3e1bdd55fb80d50770ff0
bloodnighttw <bbeenn1227@gmail.com>
2025-05-07 17:32:04 +0800

    chore: add translate to top panel button.

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -61,7 +61,7 @@ export default async function RootLayout({
 						<div className="font-bold">bloodnighttw</div>
 					</Link>
 					<div className="mr-auto"></div>
-					<PanelButton href={"/"} text={"Friends"}>
+					<PanelButton href={"/"} text={t("friends")}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
 							 className="bi bi-people" viewBox="0 0 16 16">
 							<path
@@ -69,7 +69,7 @@ export default async function RootLayout({
 						</svg>
 					</PanelButton>
 
-					<PanelButton href={"/blog"} text={"Blog"}>
+					<PanelButton href={"/blog"} text={t("Blog")}>
 						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
 							 className="bi bi-vector-pen" viewBox="0 0 16 16">
 							<path fillRule="evenodd"
@@ -78,18 +78,7 @@ export default async function RootLayout({
 						</svg>
 					</PanelButton>
 
-					<PanelButton href={"/tags"} text={"Tags"}>
-						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-							 className="bi bi-tags" viewBox="0 0 16 16">
-							<path
-								d="M3 2v4.586l7 7L14.586 9l-7-7zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586z" />
-							<path
-								d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1z" />
-						</svg>
-					</PanelButton>
-
 					<ChangeLanguage />
-
 
 				</NavPanel>
 
