118802f364f9a02e45f717e2acae2556c7fc4248
bloodnighttw <bbeenn1227@gmail.com>
2025-05-07 13:50:02 +0800

    chore: remove unused style.

--- compoments/panel/PanelButton.tsx
+++ compoments/panel/PanelButton.tsx
@@ -13,7 +13,7 @@ export default function PanelButton(props: Props) {
 	return <Link href={props.href} className={cn(
 		"rounded border border-secondary/25",
 		"hover:border-secondary/40 h-8 px-4 hover:-translate-y-0.25 duration-200",
-		"hidden sm:flex items-center justify-center text-sm gap-2 font-mono align-[12em]",
+		"hidden sm:flex items-center justify-center text-sm gap-2 font-mono",
 		"*:fill-secondary *:text-secondary hover:*:fill-primary hover:*:text-primary *:h-4",
 	)}>
 		<div className="flex gap-1.5">
