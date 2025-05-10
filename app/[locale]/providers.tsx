428d7b356e1c093086777d277dbaa798bcb9173f
bloodnighttw <bbeenn1227@gmail.com>
2025-05-10 13:18:13 +0800

    fix: fix in some case that animation will trigger when it shouldn't or not trigger when it should.

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -1,16 +1,42 @@
 'use client';
 
 import { ProgressProvider } from '@bprogress/next/app';
-import React from "react";
-import { Bar, Progress } from "@bprogress/next";
+import React, { useEffect } from "react";
+import { Bar, Progress, useProgress } from "@bprogress/next";
+
+const PopStateHandler = () => {
+
+	// since useProgress is only available in ProgressProvider, we need to create
+	// a empty component to use it1
+	const { start } = useProgress();
+
+	useEffect(() => {
+		const handlePopstate = () => {
+			console.log('popstate');
+			start()
+		}
+
+		window.addEventListener('popstate', handlePopstate);
+
+		return () => {
+			window.removeEventListener('popstate', handlePopstate);
+		}
+
+	}, [start]);
+
+	return null;
+}
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
+
+
 	return (
 		<ProgressProvider
 			options={{
 				template:null
 			}}
 		>
+			<PopStateHandler />
 			<Progress>
 				<Bar className="!bg-primary/60 !top-16"/>
 			</Progress>
