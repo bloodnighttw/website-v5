c7a2018fc28a3240894d3bc54a8f3abcbe235db1
bloodnighttw <bbeenn1227@gmail.com>
2025-05-10 16:20:14 +0800

    fix: page enter animation.

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -16,5 +16,5 @@ export default function Template({ children }: { children: React.ReactNode }) {
 		}
 	}, []);
 
-	return <div>{children}</div>
+	return <div className="page-enter">{children}</div>
 }
