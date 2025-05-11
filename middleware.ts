fbc9584edfa6d90a2acd9ee1271e86d6f68f2654
bloodnighttw <bbeenn1227@gmail.com>
2025-05-11 17:07:59 +0800

    feat: add middleware when no locale, based on next-intl middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,19 @@
 import createMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
 
-export default createMiddleware(routing);
+export default createMiddleware({
+	// A list of all locales that are supported
+	locales: routing.locales,
+
+	// Used when no locale matches
+	defaultLocale: routing.defaultLocale,
+
+	// This function is called when a request is made to a page without a locale prefix
+	localeDetection: true,
+
+	// This function is called for each request to determine the locale
+	localePrefix: 'always'
+});
 
 export const config = {
 	// Match all pathnames except for
