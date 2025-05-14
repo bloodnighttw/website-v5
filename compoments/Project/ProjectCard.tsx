325c0fc3a991902e509eb1962c9d695c89d543e3
bloodnighttw <bbeenn1227@gmail.com>
2025-05-14 17:27:07 +0800

    refactor: refactor border part.

--- compoments/Project/ProjectCard.tsx
+++ compoments/Project/ProjectCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Github } from "@/app/assets/svg";
 import { svgUrl } from "@/utils/constant";
 import Link from "@/i18n/navigation";
-import GlareCard from "@/compoments/GlareCard";
+import GlareCard, { GlareCardBorder } from "@/compoments/GlareCard";
 
 interface Props {
 	name: string;
@@ -19,40 +19,45 @@ export default function ProjectCard(props: Props) {
 			href={"/project/" + props.name}
 			className="w-full h-full"
 		>
-			<GlareCard strength={1} className="rounded bg-secondary/20 h-full shadow"
-					   childrenClassName="p-4 bg-gradient-to-br from-bprimary/90 to-bsecondary/90 gap-2">
-				<div className="flex gap-2 items-start text-center align-baseline">
-				<span>
-					{isGithub ? (
-						Github
-					) : (
-						<Image
-							src={props.link}
-							alt={props.name}
-							width={24}
-							height={24}
-						/>
-					)}
-				</span>
-					<p className="text-xl font-mono align-text-bottom">{props.name}</p>
-					<div className="ml-auto"></div>
+			<GlareCard strength={1} className="bg-secondary/10 rounded-[0.5rem]">
+				<GlareCardBorder className="rounded *:first:rounded-t *:last:rounded-b">
+					<div className="p-2 from-bsecondary/90 to-bprimary/90 bg-gradient-to-br">
 
-					{props.stack
-						.filter((it) => it in svgUrl)
-						.map((stack) => {
-							return (
-								<Image
-									key={stack}
-									src={svgUrl[stack]}
-									alt={stack}
-									width={24}
-									height={24}
-								/>
-							);
-						})}
-				</div>
-				<div className="flex mt-2 gap-2 rounded"></div>
-				<p className="text-secondary mt-2">{props.description}</p></GlareCard>
+						<div className="flex gap-2 items-start text-center">
+							<span>
+								{isGithub ? (
+									Github
+								) : (
+									<Image
+										src={props.link}
+										alt={props.name}
+										width={24}
+										height={24}
+									/>
+								)}
+							</span>
+							<p className="text-xl font-mono align-text-bottom">{props.name}</p>
+							<div className="ml-auto"></div>
+
+							{props.stack
+								.filter((it) => it in svgUrl)
+								.map((stack) => {
+									return (
+										<Image
+											key={stack}
+											src={svgUrl[stack]}
+											alt={stack}
+											width={24}
+											height={24}
+										/>
+									);
+								})}
+						</div>
+						<div className="flex mt-2 gap-2 rounded"></div>
+						<p className="text-secondary mt-2">{props.description}</p>
+					</div>
+				</GlareCardBorder>
+			</GlareCard>
 		</Link>
 	);
 }
