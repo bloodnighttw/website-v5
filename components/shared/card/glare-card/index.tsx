adb9112f8c59a40683240329489f062a6d6745e6
bloodnighttw <bbeenn1227@gmail.com>
2025-05-22 00:03:01 +0800

    refactor: remove unused ReactNode import from GlareCard component

--- components/shared/card/glare-card/index.tsx
+++ components/shared/card/glare-card/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef, ReactNode } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import cn from "@/utils/cn";
 
 interface Props {
