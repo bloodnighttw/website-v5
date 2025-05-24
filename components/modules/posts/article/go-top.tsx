8b0aff2693ff1014c19ebaf0e6fb8e941160dfbc
bloodnighttw <bbeenn1227@gmail.com>
2025-05-24 12:22:36 +0800

    refactor: improve visibility logic for GoToTop component

--- components/modules/posts/article/go-top.tsx
+++ components/modules/posts/article/go-top.tsx
@@ -7,14 +7,20 @@ interface GoToTopProps {
 }
 
 export default function GoToTop({progress}: GoToTopProps) {
+	
+	const [hide, setHide] = React.useState(false);
+
+	if(progress > 0 && !hide) {
+		setHide(true);
+	}
 
 	return <div
 		className={cn(
 			"cursor-pointer active:scale-90 duration-200",
 			// to prevent the animation is triggered when first load
-			progress === -1 && "hidden",
 			progress === 0 && "fade-out",
 			progress >= 1 && "fade-in",
+			!hide && "hidden"
 		)}
 		// bring the element to the top
 		onClick={(e)=> {
