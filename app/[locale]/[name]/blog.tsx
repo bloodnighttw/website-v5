544fa4302d8b49949eedeadeb8ec69c3a36ec7f1
bloodnighttw <bbeenn1227@gmail.com>
2025-05-30 22:45:27 +0800

    feat: update BlogPosts component to fetch posts based on language preference

--- app/[locale]/[name]/blog.tsx
+++ app/[locale]/[name]/blog.tsx
@@ -1,14 +1,16 @@
-import { allPosts } from "content-collections";
 import DotBackground from "@/components/shared/dot-background";
 import Part from "@/components/shared/part";
 import CardCollection from "@/components/modules/posts/section/collection";
 import { PostCards } from "@/components/modules/posts/section/card";
 import Text from "./Text"
+import { getTranslations } from "next-intl/server";
+import { allPostWithEnPriority, allPostWithZhPriority } from "@/utils/allpost";
 
 export default async function BlogPosts() {
-	const metadataWithPreview = allPosts.sort(
-		(a, b) => b.date.getTime() - a.date.getTime(),
-	);
+	const t = await getTranslations();
+	const lang = t("lang");
+
+	const allPost = lang === "en" ? allPostWithEnPriority : allPostWithZhPriority;
 
 	return (
 		<div className="page-enter">
@@ -17,7 +19,7 @@ export default async function BlogPosts() {
 			</DotBackground>
 			<Part className="gradient-background">
 				<CardCollection>
-					<PostCards infos={metadataWithPreview} />
+					<PostCards infos={allPost} />
 				</CardCollection>
 			</Part>
 		</div>
