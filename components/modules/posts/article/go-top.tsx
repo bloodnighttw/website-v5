50bb1fd0ee663318c47dbac8b53a41a84b836b56
bloodnighttw <bbeenn1227@gmail.com>
2025-06-04 19:12:08 +0800

    refactor: improve visibility logic for GoToTop component with fade-out animation

--- components/modules/posts/article/go-top.tsx
+++ components/modules/posts/article/go-top.tsx
@@ -7,14 +7,32 @@ import { useProgress } from "@/utils/hooks/toc";
 export default function GoToTop() {
 
 	const progress = useProgress();
+	const [hidden, setHidden] = React.useState(true);
+	const progressEqualsZero = progress === 0;
+
+	React.useEffect(() => {
+		let id = null;
+		if (progressEqualsZero) {
+			id = setTimeout(() => {
+				setHidden(true);
+			}, 400); // delay to allow fade-out animation
+
+		} else {
+			setHidden(false);
+		}
+		return () => {
+			if (id) clearTimeout(id);
+		};
+	}, [progressEqualsZero]);
+
 
 	return <div
 		className={cn(
 			"cursor-pointer active:scale-90 duration-200",
 			// to prevent the animation is triggered when first load
-			progress === -1 && "hidden",
 			progress === 0 && "fade-out",
 			progress >= 1 && "fade-in",
+			hidden && "hidden",
 		)}
 		// bring the element to the top
 		onClick={(e)=> {
